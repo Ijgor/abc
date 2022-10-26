0e67a041650a727e5eb5e480aeebe45f8ca88814
Ijgor <104577376+Ijgor@users.noreply.github.com>
2022-10-26 19:40:28 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,467 @@
+﻿using System;
+using static System.Math;
+
+namespace ConsoleApp7
+{
+    public class xxxx
+    {
+        internal static double add1()
+        {
+            Console.WriteLine("Enter a and b");
+            double a = double.Parse(Console.ReadLine());
+
+
+            return a * a;
+
+
+
+        }
+        internal static double add2()
+        {
+            Console.WriteLine("Enter a and b");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            return a * b;
+
+
+
+        }
+        internal static double add3()
+        {
+            Console.WriteLine("Enter a and h");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+
+            return (a * b) / 2;
+
+
+
+        }
+        internal static double add4()
+        {
+            Console.WriteLine("Enter a and h");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+
+            return (a * b);
+
+
+
+        }
+        internal static double add5()
+        {
+            Console.WriteLine("Enter a and h");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+
+            return (a * b);
+
+
+
+        }
+        internal static double add6()
+        {
+            Console.WriteLine("Enter przekątna 1 and 2");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            return (a * b);
+
+
+
+        }
+        internal static double add7()
+        {
+            Console.WriteLine("Enter przekątna 1 and 2");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            return (a * b) / 2;
+
+
+
+        }
+
+        internal static double add9()
+        {
+            Console.WriteLine("Enter a and b and h");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+
+            return ((a + b) * c) / 2;
+
+
+
+        }
+        internal static double add10()
+        {
+            Console.WriteLine("Enter a and b");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            return (a * b) / 2;
+
+
+
+        }
+        internal static double add11()
+        {
+            Console.WriteLine("Enter a and b and h");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+
+            return ((a + b) * c) / 2;
+
+
+
+        }
+        internal static double add12()
+        {
+            Console.WriteLine("Enter a,h and H");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            return (a * b) * c / 3;
+
+
+
+        }
+        internal static double add13()
+        {
+            Console.WriteLine("Enter Pp and a");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+            return a / b;
+
+
+
+        }
+        internal static double add14()
+        {
+            Console.WriteLine("Enter a,h and H");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            return (a * b) * c / 3;
+
+
+
+        }
+        internal static double add15()
+        {
+            Console.WriteLine("Enter a");
+            double a = double.Parse(Console.ReadLine());
+
+            return a * a * a;
+
+
+
+        }
+        internal static double add16()
+        {
+            Console.WriteLine("Enter a,b and h");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            return a * b * c;
+
+
+
+        }
+        internal static double add17()
+        {
+            Console.WriteLine("Enter PP and a");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            return a / b;
+
+
+
+        }
+        internal static double add18()
+        {
+            Console.WriteLine("Enter PP ");
+            double a = double.Parse(Console.ReadLine());
+
+
+            return Sqrt(a);
+
+
+
+        }
+        internal static double add19()
+        {
+            Console.WriteLine("Enter PP and b");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            return a / b;
+
+
+
+        }
+        internal static double add20()
+        {
+            Console.WriteLine("Enter PP and h");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            return 2 * a / b;
+
+
+
+        }
+        internal static double add21()
+        {
+            Console.WriteLine("Enter Pp,a and b");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            return a / (b * c);
+
+
+
+        }
+        internal static double add22()
+        {
+
+
+
+            Console.WriteLine("Enter Pp,a and h");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            return 2 * a / (b * c);
+
+
+
+        }
+        internal static double add23()
+        {
+            Console.WriteLine("Enter Pp,a and b");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            return 2 * a / (b + c);
+
+
+
+        }
+        internal static double add24()
+        {
+            Console.WriteLine("Enter Pp and h");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            return 2 * a / b;
+
+
+
+        }
+        internal static double add25()
+        {
+            Console.WriteLine("Enter Pp and przekątna 1");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            return a / b;
+
+
+
+        }
+        internal static double add26()
+        {
+            Console.WriteLine("Enter a");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+            return a * a * 6;
+
+
+
+        }
+        internal static double add27()
+        {
+            Console.WriteLine("Enter m and g");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            return a * b;
+
+
+
+        }
+        internal static double add28()
+        {
+            Console.WriteLine("Enter m and r");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            return a * b * b / 2;
+
+
+
+        }
+        internal static double add29()
+        {
+            Console.WriteLine("Enter s and h");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            return a / b;
+
+
+
+        }
+        internal static double add30()
+        {
+            Console.WriteLine("Enter m and v");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            return a / b;
+
+
+
+        }
+        internal static double add31()
+        {
+            Console.WriteLine("Enter n and t");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            return a / b;
+
+
+
+        }
+        internal static double add32()
+        {
+            Console.WriteLine("Enter d and v");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            return a * b * 10;
+
+
+
+        }
+        internal static double add33()
+        {
+            Console.WriteLine("Enter v and t");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            return a / b;
+
+
+
+        }
+        internal static double add34()
+        {
+            Console.WriteLine("Enter a and t");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            return a * b * b / 2;
+
+
+
+        }
+        internal static double add35()
+        {
+            Console.WriteLine("Enter a and b");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+            return a * a * 6;
+
+
+
+        }
+        internal static double add36()
+        {
+            Console.WriteLine("Enter a and b and c");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            return a + b + c;
+
+
+
+        }
+        internal static double add37()
+        {
+            Console.WriteLine("Enter a");
+            double a = double.Parse(Console.ReadLine());
+            return 4 * a;
+
+
+
+        }
+        internal static double add38()
+        {
+            Console.WriteLine("Enter a and b");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            return (a + b) * 2;
+
+
+
+        }
+        internal static double add39()
+        {
+            Console.WriteLine("Enter a and b and c and d");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double d = double.Parse(Console.ReadLine());
+            return a + b + c + d;
+
+
+
+        }
+        internal static double add40()
+        {
+            Console.WriteLine("Enter a");
+            double a = double.Parse(Console.ReadLine());
+            return a * a * a;
+
+
+
+        }
+        internal static double add42()
+        {
+            Console.WriteLine("Enter S and t");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            return a / b;
+
+
+
+        }
+        internal static double add43()
+        {
+            Console.WriteLine("Enter F and S");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            return a / b;
+
+
+
+
+        }
+        internal static double add44()
+        {
+            Console.WriteLine("Enter f");
+            double a = double.Parse(Console.ReadLine());
+            return 2 * PI * a;
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            Console.WriteLine("1- kwadrat \r\n2- prostokąt\r\n3- trójkąt\r\n4- równoległobok\r\n5- romb (bok, wysokość)\r\n6- deltoida\r\n7- romb (przekątne)\r\n8- trapez prostokątny\r\n9- trójkąt prostokątny\r\n10- trapez\r\n11- (V)ostrosłup czworokątny");
+            Console.WriteLine("12- wysokość trójkąta(Pp,a) \r\n13- czworościan foremny(a,h,H) \r\n14- (V)sześcian\r\n15- (V)prostopadłościan\r\n16-  wysokość rombu (bok, Pp)\r\n17- bok kwadratu\r\n18- bok prostokąta\r\n19- podstawa trójkąta\r\n20- wysokość równoległoboku\r\n21- podstawy (a+b) równoległoboku\r\n22- wysokość trapezu");
+            Console.WriteLine("23- bok trapezu \r\n24- przekątna rombu \r\n25- Pp sześcianu\r\n26- siła grawitacji\r\n27- moment bezwladnoci wlaca\r\n28- objętość\r\n29- gęstość\r\n30- częstotliwość\r\n31- siła wyporu\r\n32- przyspieszenie\r\n33- droga\r\n34- obw trójkąta\r\n35-obw kwadratu \r\n36-obw prostokata\r\n37- obw trapezu\r\n38- obj szescianu\r\n39- predkosc liniowa\r\n40- cisnienie\r\n41- predkosc katowa");
+            string inp = Console.ReadLine().ToLower();
+            if (inp == "1")
+            {
+                Console.WriteLine("pole kwadrata: " + xxxx.add1());
+
+            }
+            else if (inp == "2")
+            {
+
+                Console.WriteLine("pole prostokonta: " + xxxx.add2());
+
+            }
+            else if (inp == "3")
+            {
+
+                Console.WriteLine("pole trójkąta: " + xxxx.add3());
+            }
+            else if (inp == "4")
+            {
+
+                Console.WriteLine("pole równoległoboku: " + xxxx.add4());
+            }
+            else if (inp == "5")
+            {
+                Console.WriteLine("pole rombu: " + xxxx.add5());
+            }
+            else if (inp == "6")
+            {
+                Console.WriteLine("pole deltoidu: " + xxxx.add6());
+            }
+            else if (inp == "7")
+            {
+                Console.WriteLine("pole rombu: " + xxxx.add7());
+            }
+
+
+            else if (inp == "8")
+            {
+                Console.WriteLine("pole trapezu: " + xxxx.add9());
+            }
+            else if (inp == "9")
+            {
+                Console.WriteLine("pole trójkąta prostokątnego: " + xxxx.add10());
+            }
+            else if (inp == "10")
+            {
+                Console.WriteLine("pole trapezu: " + xxxx.add11());
+            }
+            else if (inp == "11")
+            {
+                Console.WriteLine("pole ostrosłupa czworokątnego: " + xxxx.add12());
+            }
+            else if (inp == "12")
+            {
+                Console.WriteLine("wysokość trójkąta: " + xxxx.add13());
+            }
+            else if (inp == "13")
+            {
+                Console.WriteLine("pole czworościanu foremnego: " + xxxx.add14());
+            }
+            else if (inp == "14")
+            {
+                Console.WriteLine(" pole sześcianu: " + xxxx.add15());
+            }
+            else if (inp == "15")
+            {
+                Console.WriteLine("pole prostopadłościanu: " + xxxx.add16());
+            }
+            else if (inp == "16")
+            {
+                Console.WriteLine("pole rombu: " + xxxx.add17());
+            }
+            else if (inp == "17")
+            {
+                Console.WriteLine("bok kwadratu: " + xxxx.add18());
+            }
+            else if (inp == "18")
+            {
+                Console.WriteLine("bok prostokąta: " + xxxx.add19());
+            }
+            else if (inp == "19")
+            {
+                Console.WriteLine("podstawa trójkąta: " + xxxx.add20());
+            }
+            else if (inp == "20")
+            {
+                Console.WriteLine("wysokość równoległoboku: " + xxxx.add21());
+            }
+            else if (inp == "21")
+            {
+                Console.WriteLine("podstawy trapezu (a+b): " + xxxx.add22());
+            }
+            else if (inp == "22")
+            {
+                Console.WriteLine("wysokość trapezu: " + xxxx.add23());
+            }
+            else if (inp == "23")
+            {
+                Console.WriteLine("bok trapezu: " + xxxx.add24());
+            }
+            else if (inp == "24")
+            {
+                Console.WriteLine("przekątna rombu: " + xxxx.add25());
+            }
+            else if (inp == "25")
+            {
+                Console.WriteLine("pole powierzchni sześcianu: " + xxxx.add26());
+            }
+            else if (inp == "26")
+            {
+                Console.WriteLine("siła grawitacji: " + xxxx.add27());
+            }
+            else if (inp == "27")
+            {
+                Console.WriteLine("moment bezwladnoci wlaca: " + xxxx.add28());
+            }
+            else if (inp == "28")
+            {
+                Console.WriteLine("objętość: " + xxxx.add29());
+            }
+            else if (inp == "29")
+            {
+                Console.WriteLine("gęstość: " + xxxx.add30());
+            }
+            else if (inp == "30")
+            {
+                Console.WriteLine("częstotliwość: " + xxxx.add31());
+            }
+            else if (inp == "31")
+            {
+                Console.WriteLine("siła wyporu: " + xxxx.add32());
+            }
+            else if (inp == "32")
+            {
+                Console.WriteLine("przyspieszenie: " + xxxx.add33());
+            }
+            else if (inp == "33")
+            {
+                Console.WriteLine("droga: " + xxxx.add34());
+            }
+            else if (inp == "34")
+            {
+                Console.WriteLine("pole powierzchni sześcianu: " + xxxx.add35());
+            }
+            else if (inp == "35")
+            {
+                Console.WriteLine("obw trójkąt: " + xxxx.add36());
+            }
+            else if (inp == "36")
+            {
+                Console.WriteLine("obw kwadratu: " + xxxx.add37());
+            }
+            else if (inp == "37")
+            {
+                Console.WriteLine("obw prostokąta: " + xxxx.add38());
+            }
+            else if (inp == "38")
+            {
+                Console.WriteLine("obw trapezu: " + xxxx.add39());
+            }
+            else if (inp == "39")
+            {
+                Console.WriteLine("objetosc szescianu: " + xxxx.add40());
+            }
+            else if (inp == "40")
+            {
+                Console.WriteLine("predkosc liniowa: " + xxxx.add41());
+            }
+            else if (inp == "41")
+            {
+                Console.WriteLine("cisnienie: " + xxxx.add42());
+            }
+            else if (inp == "42")
+            {
+                Console.WriteLine("prędkość kątowa: " + xxxx.add43());
+            }
+                Console.ReadKey();
+
+        }
+    }
+}
